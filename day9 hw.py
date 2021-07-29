19a114a8fab0a4b93e9ef3f438ab0ca3c9fdaa9a
aaronpengfc <87631336+aaronpengfc@users.noreply.github.com>
2021-07-29 20:27:04 +0800

    Add files via upload

---
+++ day9 hw.py
@@ -0,0 +1,26 @@
+
+dic = {"A001":["gummy bear", 20],"A002":["ice bar" , 25],"A004":["instant noodles" , 10],"A006":["soda" , 30]}
+def sel_2(code):
+    h = input("your objects name:")
+    n = int(input("your objects price:"))
+    print(h)
+    print(n)
+    dic[code] = [h,n]
+while True:
+    a = int(input("the items code:1\nstop:2\nyour number:"))
+    print(a)
+    print(dic) 
+    if a==1:
+        q = input("code:")
+        print(q)
+        if q in dic:
+            print(dic[q])
+        else:
+            print("there isn't this object")
+            x = sel_2(q)
+            print(x)
+    elif a==2:
+        break
+        
+            
+            
